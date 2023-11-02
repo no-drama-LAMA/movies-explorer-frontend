46ddc071c9a8fef9d67e20e31567a8b8c4905a5a
lambkin <alinao.post@yandex.ru>
2023-11-02 17:09:25 +0300

    fix: починить всё

--- src/components/SubmitButton/SubmitButton.jsx
+++ src/components/SubmitButton/SubmitButton.jsx
@@ -1,13 +1,15 @@
 import { useContext } from 'react';
 import './SubmitButton.css'
 import CurrentUserContext from '../../contexts/CurrentUserContext';
+import { useLocation } from 'react-router-dom';
 
 function SubmitButton({buttonText, isFormValid, isLoading, isError, inputValues}) {
   const currentUser = useContext(CurrentUserContext);
+  const {pathname} = useLocation();
 
   return (
     <button 
-      className={`submit-button ${!isFormValid || isLoading || isError || (inputValues.username === currentUser.name && inputValues.email === currentUser.email) ? 'submit-button_disabled' : ' '}`}
+      className={`submit-button ${!isFormValid || isLoading || isError || (pathname === '/profile' && inputValues.username === currentUser.name && inputValues.name.email === currentUser.email) ? 'submit-button_disabled' : ' '}`}
       type="submit"
     >
       {buttonText}
